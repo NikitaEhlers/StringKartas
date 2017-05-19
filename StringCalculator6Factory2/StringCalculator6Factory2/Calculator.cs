9b9b368b02623881ee2171906e82ca6bc048e50f
Nikita Ehlers <nikita@agrista.local>
2017-05-19 11:02:48 +0200

    Second factory iteration

---
+++ StringCalculator6Factory2/StringCalculator6Factory2/Calculator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator6Factory2
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            if (numbers == "")
+            {
+                return 0;
+            }
+
+            string[] splitNumbers = CreateSplitNumbers(numbers);
+
+            CheckForNegative(splitNumbers);
+
+            return CalculateTotal(splitNumbers);
+        }
+
+        public string[] CreateSplitNumbers(string numbers)
+        {
+            DelimiterFactory factory = new ConcreteDelimiterFactory();
+            IDelimiter splitter = factory.CreateDelimiter(numbers);
+            string[] delimiter = splitter.CreateDelimiter(numbers);
+            IDelimiter numberFormat = factory.GetFormattedNumbers(numbers);
+            numbers = numberFormat.GetFormattedNumbers(numbers);
+
+            string[] splitNumbers = numbers.Split(delimiter, StringSplitOptions.None);
+
+            return splitNumbers;
+        }
+
+        public int CalculateTotal(string[] splitNumbers)
+        {
+            int total = 0;
+
+            foreach (var number in splitNumbers)
+            {
+                int num = Convert.ToInt32(number);
+
+                if (num > 1000)
+                {
+                    num = 0;
+                }
+
+                total = total + num;
+            }
+
+            return total;
+        }
+
+        public void CheckForNegative(string[] splitNumbers)
+        {
+            string exceptionMessage = "";
+
+            foreach (var number in splitNumbers)
+            {
+                if (!number.Contains("-"))
+                {
+                    continue;
+                }
+
+                if (exceptionMessage == "")
+                {
+                    exceptionMessage = number;
+                }
+
+                else
+                {
+                    exceptionMessage = exceptionMessage + ", " + number;
+                }
+            }
+
+            if (exceptionMessage != "")
+            {
+                throw new Exception("Negatives not allowed " + exceptionMessage);
+            }
+        }
+    }
+
+    
+}
---
+++ StringCalculator6Factory2/StringCalculator6Factory2/IDelimiter.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator6Factory2
+{
+    public interface IDelimiter
+    {
+        string[] CreateDelimiter(string numbers);
+        string GetFormattedNumbers(string numbers);
+    }
+
+    public class DefaultDelimiter : IDelimiter
+    {
+        public string[] CreateDelimiter(string numbers)
+        {
+            string[] delimiter = new string[] { ",", "\n" };
+            return delimiter;
+        }
+
+        public string GetFormattedNumbers(string numbers)
+        {
+            return numbers;
+        }
+    }
+
+    public class CustomDelimiter : IDelimiter
+    {
+        public string[] CreateDelimiter(string numbers)
+        {
+            string[] calculationParts = numbers.Split('\n');
+            calculationParts[0] = calculationParts[0].Replace("//", "");
+            calculationParts[0] = calculationParts[0].Replace("[", "");
+            string[] delimiter = calculationParts[0].Split(']');
+
+            return delimiter;
+        }
+
+        public string GetFormattedNumbers(string numbers)
+        {
+            string[] numberParts = numbers.Split('\n');
+            return numberParts[1];
+        }
+    }
+
+    public abstract class DelimiterFactory
+    {
+        public abstract IDelimiter CreateDelimiter(string numbers);
+        public abstract IDelimiter GetFormattedNumbers(string numbers);
+    }
+
+    public class ConcreteDelimiterFactory : DelimiterFactory
+    {
+        public override IDelimiter CreateDelimiter(string numbers)
+        {
+            string delimiter = "Default";
+
+            if (numbers.Contains("//"))
+            {
+                delimiter = "Custom";
+            }
+
+            switch (delimiter)
+            {
+                case "Default":
+                    {
+                        return new DefaultDelimiter();
+                    }
+
+                case "Custom":
+                    {
+                        return new CustomDelimiter();
+                    }
+
+                default:
+                    {
+                        throw new Exception();
+                    }
+            }
+
+            
+        }
+
+        public override IDelimiter GetFormattedNumbers(string numbers)
+        {
+            string delimiter = "Default";
+
+            if (numbers.Contains("//"))
+            {
+                delimiter = "Custom";
+            }
+
+            switch (delimiter)
+            {
+                case "Default":
+                    {
+                        return new DefaultDelimiter();
+                    }
+
+                case "Custom":
+                    {
+                        return new CustomDelimiter();
+                    }
+
+                default:
+                    {
+                        throw new Exception();
+                    }
+            }
+
+            
+        }
+    }
+}
